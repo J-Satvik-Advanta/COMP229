dafac87ed40cfef5a3cdb9d0303a9beb0c796ffc
Satvik Kumar Jain <satvik.jain@advantawitty.com>
2024-05-31 20:03:22 -0400

    added folder structure and packages

---
+++ src/pages/footer/footer.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer footer-transparent d-print-none" style={{height: '10%'}}>
+    <div className="container-xl">
+      <div className="row text-center align-items-center justify-content-center">
+        <div className="col-12 col-lg-auto mt-3 mt-lg-0">
+          <ul className="list-inline list-inline-dots mb-0">
+            <li className="list-inline-item">
+              Copyright © 2023 All rights reserved.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
